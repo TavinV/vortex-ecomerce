0e114d4aea0a3f0a0d3bf77e56bb54cbc7b1c4aa
TavinV <otavioviniciusads@gmail.com>
2024-09-21 17:05:07 -0300

    funcionalidade de produtos não disponíveis

--- script.js
+++ script.js
@@ -11,40 +11,40 @@ function criarElementoHtml(html){
 
 function carregar_produto_por_JSON(produto){
     return criarElementoHtml(`
-       <div class="produto">
+       <a target="_blank" href="${produto.link_produto}" class="produto">
           <div class="img-container">
-          <img class="${produto.img_class}"  src="${produto.imagem}"></img>
-          </div>
-          <span class="informacoes-produto">
+            <img class="${produto.img_class}"  src="${produto.imagem}"></img>
+            </div>
+            <span class="informacoes-produto">
             <h2>${produto.nome}</h2>
-            <p>R$ ${produto.valor}</p>
-          </span>
-
-          ${produto["frete-gratis"] ? `
-          <div class="propriedade">
-            Frete grátis
-            <i class="bx bx-package"></i>
-          </div>` : `
-          <div class="propriedade">
-            Envio para todo o país
-            <i class='bx bxs-plane-alt'></i>
-          </div>`}
-
-          <div class="propriedade">
-            ${produto["cores-disponiveis"]} Cores disponíveis
-            <i class="bx bxs-adjust-alt"></i>
-          </div>
-          <button>Comprar Agora</button>
-        </div>
-        `)
-}
-
-
-fetch(endereco_dados).then(resposta => resposta.json()).then(dados =>{
-    console.log(dados)
-    // Carregando as informações e adicionando à aba de produtos
-    
-    total_produtos = 0
+            </span>
+            
+            <p class="valor">  R$ ${produto.valor}</p>
+            ${produto["frete-gratis"] ? ` 
+              <div class="propriedade">
+                Frete grátis
+                <i class="bx bx-package"></i>
+            </div>` : `
+            <div class="propriedade">
+              Envio para todo o país
+              <i class='bx bxs-plane-alt'></i>
+            </div>`} 
+            
+            <div class="propriedade">
+              ${produto["cores-disponiveis"]} Cores disponíveis
+              <i class="bx bxs-adjust-alt"></i>
+            </div>
+            <button>Comprar Agora</button>
+            </a>
+            `)
+          }
+          
+          
+          fetch(endereco_dados).then(resposta => resposta.json()).then(dados =>{
+            console.log(dados)
+            // Carregando as informações e adicionando à aba de produtos
+            
+            total_produtos = 0
     dados.forEach(produto => {
         total_produtos++
         document.getElementById("produtos").append(carregar_produto_por_JSON(produto))
