830f75e6f6d00954fb822e9b79818049d6780ad9
TavinV <otavioviniciusads@gmail.com>
2024-09-22 00:38:47 -0300

    ABSOLUTE RESPONSIVE

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-function abrir_menu(){
+var menu_aberto = false
+var search_aberto = false
 
-    document.getElementById('links').classList.toggle('links-active')
+const links_EL = document.getElementById('links-active')
+const mobile_search_EL = document.getElementById('mobile-search-bar')
+
+
+function abrir_menu(){
+    if(search_aberto){
+        search_aberto = false
+        menu_aberto = true
+        
+        mobile_search_EL.classList.toggle('active') // Caso o search esteja aberto, fecha-lo para abrir o menu
+        links_EL.classList.toggle('active')
+    } else {
+        menu_aberto = true
+        links_EL.classList.toggle('active') // Caso o search esteja fechado, abrir menu normalmente
+    }
+}
+
+function abrir_search(){
+    if (menu_aberto){
+        menu_aberto = false
+        search_aberto = true
+        
+        links_EL.classList.toggle('active') // Caso o menu esteja aberto, fecha-lo antes de abir o search
+        mobile_search_EL.classList.toggle('active')
+    } else {
+        search_aberto = true
+        mobile_search_EL.classList.toggle('active') // Caso o menu esteja fechado, abrir o search
+    }
 }
